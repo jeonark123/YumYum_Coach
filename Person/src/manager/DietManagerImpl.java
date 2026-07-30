76ab7b7e978a12970afdf50d971e6e2d0c2d35ce
YERIN0804 <dpfls0570@gmail.com>
2026-07-30 14:02:18 +0900

    식단 클래스 초안

---
+++ Person/src/manager/DietManagerImpl.java
@@ -0,0 +1,45 @@
+package manager;
+
+import java.util.ArrayList;
+import domain.Diet;
+import java.util.Date;
+import java.util.List;
+
+public class DietManagerImpl implements DietManager {
+	
+	private List<Diet> dietList = new ArrayList<>();
+	private final int MAX_SIZE = 100;
+	private static DietManagerImpl dm = new DietManagerImpl();
+	
+	public DietManagerImpl() {
+	}
+	
+	public static DietManagerImpl getInstance() {
+		return dm;
+	}
+	
+	@Override
+	public void addDiet(Diet diet) {
+		
+	}
+	
+	@Override
+	public Diet[] getDietList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public Diet[] searchByDietDate(Date dietDate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public int getTotalKcal() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+}
